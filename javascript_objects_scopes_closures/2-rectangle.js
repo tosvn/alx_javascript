f2112a55cc8aa78e6e38e7776702a5db2f97c072
Mudasiru Abdulsalam <108347465+tosvn@users.noreply.github.com>
2023-09-20 15:17:42 -0700

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,8 @@
-// create a class with its instructor
+#!/usr/bin/node
+// create a class with its constructor
 class Rectangle {
-	constructor(w, h) {
-		if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      // If w or h is <= 0 or not a number, create an empty object
-      return {};
-    } else {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
