d6b842e981f898e23cf721ce0cad63d17f870311
Abdulsalam <abdulsalammudasiru2021@gmail.com>
2023-10-01 00:38:30 -0700

    3-request

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const request = require("request");
+
+const url = process.argv[2];
+const file_path = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  fs.writeFile(file_path, body, "utf8", function (error) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+  });
+});
