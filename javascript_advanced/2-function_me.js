ab94b94932526a423c9a1b48327d7bf7bf2b1561
Abdulsalam <abdulsalammudasiru2021@gmail.com>
2023-10-01 10:04:58 -0700

    2-function

---
+++ javascript_advanced/2-function_me.js
@@ -0,0 +1,9 @@
+//creating a function used to send a message in the browser using alert
+function welcomeMessage(fullname) {
+	alert("Welcome " + fullname)
+}
+
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
+
